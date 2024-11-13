691ec20fc6831c679eab3dcfc0b8d296859aeb1c
TijnDriessen <tej.driessen@student.han.nl>
2024-11-13 18:46:38 +0100

    Create Insert & delete tabellen.sql

---
+++ Insert & delete tabellen.sql
@@ -0,0 +1,9 @@
+INSERT INTO Locatie (locatiecode, naam, adres, postcode, plaats, land)
+VALUES ('SCHIPHAIRP', 'autoverhuur Heijendaal', 'Hoofdweg 27', '1276GH', 'Amsterdam', 'Nederland' );
+
+DELETE FROM Locatie;
+
+INSERT INTO Klant (klantnr, voornaam, tussenvoegsels, achternaam, emailadres)
+VALUES ('165321', 'Sjaak', 'van de', 'Hoek', 'sjakievandehoek@mailinator.com');
+
+/*DELETE FROM Klant;*/
