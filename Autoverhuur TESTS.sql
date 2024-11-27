bcca6f40846d4f66a4a86411254b82cf56d97521
TijnDriessen <tej.driessen@student.han.nl>
2024-11-27 16:38:25 +0100

    .

--- Autoverhuur TESTS.sql
+++ Autoverhuur TESTS.sql
@@ -1,4 +1,4 @@
-Update auto
+	Update auto
 Set kleur = 'Blauw'
 where kenteken = 'FG-SR-79';
 
@@ -52,21 +52,94 @@ Where contractnr = 994450
 
 
 
-insert into Auto (autonr, autotype, kenteken, merk, modelnaam, kleur, is_automaat, is_elektrisch, is_in_orde)
-Values(12, 'GSUV5', 'FG-SR-79', 'Opel', 'Corsa', 'blauw', 0,1,1);
+/*insert into Auto (autonr, autotype, kenteken, merk, modelnaam, kleur, is_automaat, is_elektrisch, is_in_orde)
+Values(12, 'GSUV5', 'FG-SR-79', 'Opel', 'Corsa', 'blauw', 0,1,1);*/
 
 insert into Auto (autonr, autotype, kenteken, merk, modelnaam, kleur, is_automaat, is_elektrisch, is_in_orde)
 Values(12, 'GSUV5', 'FG-SR-78', 'Opel', 'Corsa', 'blauw', 0,1,1);
 
-insert into Locatie (locatiecode, naam, adres, postcode, plaats, land)
-Values('MDLBRGHD', 'autoverhuur Heijendaal Middelburg', 'Faunastraat 1', '4331 AA', 'Middelburg', 'Nederland');
+/*insert into Locatie (locatiecode, naam, adres, postcode, plaats, land)
+Values('MDLBRGHD', 'autoverhuur Heijendaal Middelburg', 'Faunastraat 1', '4331 AA', 'Middelburg', 'Nederland');*/
 
 insert into Locatie (locatiecode, naam, adres, postcode, plaats, land)
 Values('MDLBRGHD', 'autoverhuur Heijendaal Middelburg', 'Faunastraat 2', '4331 AA', 'Middelburg2', 'Nederland2');
 
-insert into Huurcontract(contractnr, klant, van_datum, tot_datum, locatie_ophalen, locatie_terugbrengen, wenst_autotype, wenst_automaat, wenst_elektrisch, krijgt_auto)
-Values (994450, 165321, '2024-10-20', '2024-09-14', 'EDHVN', 'APLDRN', 'KSUV5', 0, 0,28);
+/*insert into Huurcontract(contractnr, klant, van_datum, tot_datum, locatie_ophalen, locatie_terugbrengen, wenst_autotype, wenst_automaat, wenst_elektrisch, krijgt_auto)
+Values (994450, 165321, '2024-10-20', '2024-09-14', 'EDHVN', 'APLDRN', 'KSUV5', 0, 0,28);*/
 
 insert into Huurcontract(contractnr, klant, van_datum, tot_datum, locatie_ophalen, locatie_terugbrengen, wenst_autotype, wenst_automaat, wenst_elektrisch, krijgt_auto)
 Values (994450, 165321, '2024-10-20', '2024-12-14', 'EDHVN', 'APLDRN', 'KSUV5', 0, 0,28);
 
+
+SELECT     Huurcontract.contractnr, 
+	       Huurcontract.klant AS klantnr, 
+		   CONCAT(Klant.voornaam, ' ', Klant.tussenvoegsels, ' ', Klant.achternaam) AS volledige_naam, 
+		   Huurcontract.van_datum, 
+		   Autotype.typecode AS gewenste_autotype, 
+		   Autotype.omschrijving AS autotype_omschrijving
+
+FROM Huurcontract
+
+JOIN Klant ON Huurcontract.klant = Klant.klantnr
+
+JOIN Autotype ON Huurcontract.wenst_autotype = Autotype.typecode
+
+WHERE Huurcontract.van_datum >= '2024-01-01'
+
+ORDER BY Huurcontract.van_datum ASC;
+
+
+
+
+
+
+SELECT		Huurcontract.contractnr, 
+			Huurcontract.klant AS klantnr, 
+			CONCAT(Klant.voornaam, ' ', Klant.tussenvoegsels, ' ', Klant.achternaam) AS volledige_naam, 
+			Huurcontract.van_datum AS begindatum, 
+			Auto.kenteken, 
+			Auto.merk
+ 
+FROM    Huurcontract
+ 
+JOIN    Klant ON Huurcontract.klant = Klant.klantnr
+ 
+JOIN    Auto ON Huurcontract.krijgt_auto = Auto.autonr
+ 
+ORDER BY    Huurcontract.van_datum;
+
+
+
+
+ 
+SELECT		Auto.merk, 
+			COUNT(Huurcontract.contractnr) AS aantal_verhuurd
+
+FROM Auto
+
+JOIN  Huurcontract ON Auto.autonr = Huurcontract.krijgt_auto
+
+GROUP BY  Auto.merk
+ 
+ORDER BY  aantal_verhuurd ASC;
+
+
+
+
+
+ 
+SELECT    Locatie.locatiecode, 
+		  Locatie.naam AS locatienaam, 
+		  COUNT(Huurcontract.contractnr) AS aantal_opgehaald
+ 
+FROM    Locatie
+ 
+JOIN    Huurcontract ON Locatie.locatiecode = Huurcontract.locatie_ophalen
+ 
+WHERE   YEAR(Huurcontract.van_datum) = 2024
+
+GROUP BY     Locatie.locatiecode, Locatie.naam
+ 
+HAVING    COUNT(Huurcontract.contractnr) > 2
+
+ORDER BY  aantal_opgehaald ASC;
